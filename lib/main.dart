4f78ca72da4205246a85c607768fd10cb2c8e7f1
Deniz OZOGUL <deniz.ozogul@bilgeadam.com>
2020-08-09 20:39:39 +0300

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        // fontFamily: 'VarelaRound',
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
